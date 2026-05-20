bef157e617dfdb8fa8481327e9f6b8bfb6cecbab
ariellalgilmore <ariellalgilmore@gmail.com>
2026-05-20 08:44:24 -0700

    Migrate wu_51273125c2: front-end-developer + design principal review

--- src/routes/data.tsx
+++ src/routes/data.tsx
@@ -4,17 +4,16 @@ import {
 } from '../components';
 import { Link } from 'react-router-dom';
 
-import { Add16 } from '@carbon/icons-react';
-import { Button } from 'carbon-components-react';
+import { Add } from '@carbon/react/icons';
+import { Button } from '@carbon/react';
 
 export const Data = () => (
 	<Main>
 		<Row>
 			<Col cols={{ sm: 1 }}>
 				<Link to="/data/add">
-					<Button>
-							Add a dataset
-						<Add16 className="bx--btn__icon"/>
+					<Button renderIcon={Add}>
+						Add a dataset
 					</Button>
 				</Link>
 			</Col>
