092ca6180adbf105498d16c953beb152e80b78e0
ariellalgilmore <ariellalgilmore@gmail.com>
2026-05-20 09:31:42 -0700

    Migrate wu_c04542adb3: front-end-developer + design principal review

--- src/carbon-components.d.ts
+++ src/carbon-components.d.ts
@@ -1,7 +1,5 @@
-declare module 'carbon-components-react';
-declare module 'carbon-components-react/lib/components/UIShell';
-declare module '@carbon/icons-react';
-declare module '@carbon/icons-react/*';
+declare module '@carbon/react';
+declare module '@carbon/react/icons';
 declare module '@carbon/charts';
 declare module '@carbon/charts-react';
 declare let module: any;
