ae5c9a0a103c93bc965b0cd96a7a4db36d88808f
ariellalgilmore <ariellalgilmore@gmail.com>
2026-05-29 13:20:31 -0700

    Migrate wu_e915977372: front-end-developer + design principal review

--- src/routes/dashboard/dashboard-search.tsx
+++ src/routes/dashboard/dashboard-search.tsx
@@ -5,14 +5,14 @@ import {
 	OverflowMenu,
 	OverflowMenuItem,
 	Search
-} from 'carbon-components-react';
+} from '@carbon/react';
 
 import {
-	Add16,
-	ArrowsVertical16,
-	CheckmarkFilled16
+	Add,
+	ArrowsVertical,
+	CheckmarkFilled
 	// 🏗️🏗️🏗️ Under construction, add in later 🏗️🏗️🏗️
-	// SettingsAdjust16
+	// SettingsAdjust
 } from '@carbon/icons-react';
 
 import { css } from 'emotion';
@@ -78,7 +78,7 @@ export const DashboardSearch = ({
 		<OverflowMenu
 			className={sortButton}
 			ariaLabel='Sort chart'
-			renderIcon = {() => <ArrowsVertical16 />}
+			renderIcon = {() => <ArrowsVertical size={16} />}
 			onClick={(event: { stopPropagation: () => void; }) => { event.stopPropagation(); }}>
 			<OverflowMenuItem
 				itemText={(
@@ -86,7 +86,7 @@ export const DashboardSearch = ({
 						Newest to oldest
 						{
 							sortDirection === SortDirection.Ascending
-								? <CheckmarkFilled16 />
+								? <CheckmarkFilled size={16} />
 								: null
 						}
 					</div>
@@ -98,7 +98,7 @@ export const DashboardSearch = ({
 						Oldest to newest
 						{
 							sortDirection === SortDirection.Descending
-								? <CheckmarkFilled16 />
+								? <CheckmarkFilled size={16} />
 								: null
 						}
 					</div>
@@ -112,7 +112,7 @@ export const DashboardSearch = ({
 			title='Add new chart'
 			aria-label='Add new chart'>
 			New chart
-			<Add16 className='bx--btn__icon'/>
+			<Add size={16} className='cds--btn__icon'/>
 		</Button>
 	</div>
 );
