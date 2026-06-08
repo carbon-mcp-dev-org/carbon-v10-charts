9be8f1d092c49aded2a64c0098791562dfc3a116
ariellalgilmore <ariellalgilmore@gmail.com>
2026-06-08 14:37:08 -0700

    migrate(wu_aa38d12f41): routes/dashboard/chart-wizard

--- src/routes/dashboard/chart-wizard/choose-chart-modal.tsx
+++ src/routes/dashboard/chart-wizard/choose-chart-modal.tsx
@@ -5,8 +5,8 @@ import {
 	Modal,
 	Tag,
 	InlineNotification,
-	NotificationActionButton
-} from 'carbon-components-react';
+	ActionableNotification
+} from '@carbon/react';
 import { ChartWizardModals } from './chart-wizard';
 import { ChartType } from '../../../interfaces';
 import { generateNewChart } from './generate-new-chart';
@@ -56,7 +56,7 @@ const chartOptionTile = css`
 		max-height: 17.3125rem;
 	}
 	text-align: center;
-	.bx--tile {
+	.cds--tile {
 		padding-right: 16px;
 	}
 	margin-top: 12px;
@@ -114,7 +114,7 @@ export const ChooseChartModal = (props: ChooseChartModalProps) => {
 		<Modal
 			open={props.shouldDisplay}
 			shouldSubmitOnEnter={false}
-			selectorPrimaryFocus='.bx--tile--selectable'
+			selectorPrimaryFocus='.cds--tile--selectable'
 			onRequestSubmit={() => {
 				generateChart();
 				props.setLastVisitedModal(ChartWizardModals.CHOOSE_CHART_MODAL);
@@ -131,35 +131,22 @@ export const ChooseChartModal = (props: ChooseChartModalProps) => {
 			primaryButtonDisabled={selectedChartType === null}>
 			{
 				props.uploadedData.wasDataModified
-					? <InlineNotification
-						{ ...warningNotificationProps }
-						actions={
-							<>
-								<NotificationActionButton
-									onClick={() => {
-										props.setUploadedData({
-											data: [],
-											wasDataModified: false
-										});
-										props.setRecommendedCharts([]);
-									}}>
-									Use demo data
-								</NotificationActionButton>
-								{
-									props.uploadedData && props.uploadedData.originalData
-									&& <NotificationActionButton
-										onClick={() => {
-											props.setUploadedData({
-												data: props.uploadedData.originalData,
-												wasDataModified: false
-											});
-											props.setRecommendedCharts([]);
-										}}>
-										Use unmodified data
-									</NotificationActionButton>
-								}
-							</>
-						} />
+					? <ActionableNotification
+						lowContrast
+						kind='warning'
+						title='Uploaded data has been modified'
+						subtitle='data did not match expected format so we modified it so you can still use it'
+						actionButtonLabel='Use demo data'
+						onActionButtonClick={() => {
+							props.setUploadedData({
+								data: [],
+								wasDataModified: false
+							});
+							props.setRecommendedCharts([]);
+						}}
+						onClose={() => {}}
+						inline
+					/>
 					: null
 			}
 			<p>Choose a type of chart and click done to start editing your new chart</p>
